dce9ab8473ec3ba993dcb93e038da615413202a5
Vinicius Vieira de Santana <vinicius.vieira@fcxlabs.com.br>
2024-06-14 16:43:07 -0300

    fix: removing typo.

--- src/states/reducers/cart.reducer.ts
+++ src/states/reducers/cart.reducer.ts
@@ -9,7 +9,6 @@ export interface CartProducts { [productId: string]: number | undefined }
 export const cartReducer = (state: CartProducts, action: CartReducerAction): CartProducts => {
     const { productId, type, quantity } = action;
 
-    debugger;
     if(type === "add" && quantity > 0) {
         let _currentQuantity = state[productId] ?? 0;
         state[productId] = _currentQuantity + quantity;
