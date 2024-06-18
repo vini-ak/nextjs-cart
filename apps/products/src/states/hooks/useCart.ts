be6e436fb51de5c08f90412e3cb012fd93af3246
Vinicius Vieira de Santana <vinicius.vieira@fcxlabs.com.br>
2024-06-18 15:02:11 -0300

    style: iniciando refatoração para Page Routers

---
+++ apps/products/src/states/hooks/useCart.ts
@@ -0,0 +1,9 @@
+"use client"
+import { useContext } from "react"
+import { CartContext } from "../providers"
+
+export const useCart = () => {
+    const { items, totalItems, addToCart, removeFromCart } = useContext(CartContext);
+    
+    return { items, totalItems, addToCart, removeFromCart };
+}
---
+++ apps/products/src/states/reducers/cart.reducer.ts
@@ -0,0 +1,23 @@
+interface CartReducerAction {
+    type: 'add' | 'remove';
+    productId: string;
+    quantity: number;
+}
+
+export interface CartProducts { [productId: string]: number | undefined }
+
+export const cartReducer = (state: CartProducts, action: CartReducerAction): CartProducts => {
+    const { productId, type, quantity } = action;
+
+    if(type === "add" && quantity > 0) {
+        let _currentQuantity = state[productId] ?? 0;
+        state[productId] = _currentQuantity + quantity;
+
+        return state; // add to cart
+    } else if(type === "remove") {
+        return { ...state, [productId]: undefined  } // remove from cart
+    } 
+    
+    return state;
+    
+}
