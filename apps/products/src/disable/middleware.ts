6522569c63952d0cc5bd765a1ca82ce1af8378f3
Vinícius Vieira <vinicius.vieira@fcxlabs.com.br>
2024-06-27 17:29:45 -0300

    fix: correção do erro de CORS.

---
+++ apps/products/src/disable/middleware.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+const corsOrigins = ['*'];
+
+export function middleware(req: NextRequest) {
+    const origin = req.headers.get('origin');
+    const res = NextResponse.next();
+
+    debugger;
+
+    // Set CORS headers
+    res.headers.set('Access-Control-Allow-Credentials', 'true');
+    res.headers.set('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT');
+    res.headers.set(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    );
+
+    if (corsOrigins.includes('*') || corsOrigins.includes(origin || '')) {
+        res.headers.set('Access-Control-Allow-Origin', origin || '*');
+    }
+
+    return res;
+}
+
+export const config = {
+    matcher: '/api/:path*', // Match all API routes
+};
